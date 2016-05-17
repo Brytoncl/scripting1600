9d3d808ea58027c92d229912eba4d20255dde388
brytoncl <brytonclittle@gmail.com>
2016-05-17 17:16:10 -0600

    edits to demo script

--- Unity project (Tutorial)/Assets/Scripts/Demo.cs
+++ Unity project (Tutorial)/Assets/Scripts/Demo.cs
@@ -4,7 +4,14 @@ using System.Collections;
 public class Demo : MonoBehaviour {
 	public int health;
 
+	void Start ()
+	{
+		DamagePlayer(10);
+	}
 
-
+	void DamagePlayer(int damage)
+	{
+		health -= damage;
+	}
 } 
 
