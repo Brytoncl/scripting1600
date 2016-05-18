1ac0adafe43b120f995c945b7b83808b0ff70896
brytoncl <brytonclittle@gmail.com>
2016-05-18 11:42:40 -0600

    class edits may 18th

---
+++ Unity Project (Class)/Assets/Scripts/Vars2.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Vars2 : MonoBehaviour {
+
+
+	int jelly = 1;
+	int honey = 5;
+	int peanutbutter = 3;
+
+	float jellyjar = 0.5f;
+	float honeyjar = 1.7f;
+	float peanutbutterjar = 3.987f;
+
+	string jellyFlavor = "grape";
+	string HoneyFlavor = "clover";
+	string peanutButterStyle = "crunchy";
+
+	int MixIngredients (int ingredient1, int ingredient2){
+		return ingredient1 + ingredient2;
+	}
+
+	int MixiIngredients (int ingredient1, int ingredient2, int ingredient3){
+		return (ingredient1+ingredient2+ingredient3);
+	}
+
+	void Start () {
+		print (MixIngredients (peanutButterStyle + MixIngredients (peanutbutter)));
+		print (MixIngredients (peanutbutter, jelly));
+		print (MixIngredients (jelly, honey)); 
+	}
+}
