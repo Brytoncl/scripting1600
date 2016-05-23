94ab72f455d984553cd96dfd6a881fc4a6fff8b4
brytoncl <brytonclittle@gmail.com>
2016-05-23 10:59:46 -0600

    syntax script added

---
+++ Unity project (Tutorial)/Assets/Scripts/syntax.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class syntax : MonoBehaviour {
+	int myInt;
+
+	int MyInt (int _i, int _j) {
+		return _i + _j;
+	}
+
+	void Start () {
+		myInt = MyInt (10, 4);
+		print (myInt);
+	}
+}
