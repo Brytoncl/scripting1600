b33f00000261c0d55005a6b8b6fbb1784b17bdaa
brytoncl <brytonclittle@gmail.com>
2016-06-01 11:07:54 -0600

    new behavior script

---
+++ Unity Project (Class)/Assets/Scripts/NewBehaviourScript.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class NewBehaviourScript : MonoBehaviour {
+	int playerHealth = 200;
+	int healthBoost = 5;
+
+	int IncreaseHealth (int _a, int _b) {
+		return _a + _b;
+	}
+
+	int HealthBoostSlow (int _c,int _d) {
+		return (_c / _d);
+	}
+	void Update () {
+		//healthBoost = HealthBoostSlow (healthBoost,2);
+		playerHealth = IncreaseHealth (playerHealth, healthBoost);
+		print (playerHealth);
+	}
+
+}
