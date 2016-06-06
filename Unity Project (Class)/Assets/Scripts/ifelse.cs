56f52f80de133b026e50b51d548f9fc254046f59
brytoncl <brytonclittle@gmail.com>
2016-06-06 11:11:37 -0600

    ifesle statements

---
+++ Unity Project (Class)/Assets/Scripts/ifelse.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ifelse : MonoBehaviour {
+
+
+	public int myInt = 1;
+		
+	// Use this for initialization
+	void Start () {
+
+
+		/*if (myBool) {
+			print ("open the gates");
+		} else {
+			print ("close the gates");
+		}*/
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+		if (myInt >= 1)
+			print (myInt);
+		else
+			print (false);
+		
+	
+	}
+}
