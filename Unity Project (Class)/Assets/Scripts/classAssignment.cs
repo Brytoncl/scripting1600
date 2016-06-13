ad0eb9141308f5980afcfb66c61474993efa2a09
brytoncl <brytonclittle@gmail.com>
2016-06-13 10:14:14 -0600

    catch up

---
+++ Unity Project (Class)/Assets/Scripts/classAssignment.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class classAssignment : MonoBehaviour {
+
+	float _a = 2.1f;
+	float _b = 2.7f;
+
+	float function (float f1, float f2) {
+		return f1 + f2;
+
+	}
+	void Start () {
+		print (function (_a, _b));
+	}
+}
