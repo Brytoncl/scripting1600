c3c10b239d0c2c8a32228b78fc8c53c5fc21517e
brytoncl <brytonclittle@gmail.com>
2016-06-15 11:38:48 -0600

    while loops switch statements

---
+++ Unity Project (Class)/Assets/Scripts/MoveWithWhileLoop.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MoveWithWhileLoop : MonoBehaviour {
+    public float xDistance = 10;
+	public float yDistance = 10;
+	public float zDistance = 0;
+	Vector3 pos;
+
+	IEnumerator MoveInX () {
+		while (pos.x <= xDistance) {
+			pos.x += 0.1f;
+			print (pos.x);
+			print ("loop");
+			yield return new WaitForSeconds (0.05f);
+
+		}
+	}
+
+	void Start () {
+		StartCoroutine (MoveInX ());
+		}
+		
+	}
