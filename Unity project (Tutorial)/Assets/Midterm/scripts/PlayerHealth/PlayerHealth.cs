855eed7fd3387ff499eb89d92a3c07bb942b5d68
brytoncl <brytonclittle@gmail.com>
2016-06-28 22:41:48 -0600

    FInished Midterm RoughDraft

---
+++ Unity project (Tutorial)/Assets/Midterm/scripts/PlayerHealth/PlayerHealth.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerHealth : MonoBehaviour {
+
+	public int health = 1;
+
+	void Update () {
+		if (health <= 0) {
+			health = 0;
+			print ("Game Over THe Aliens Caught You Out Of Ammo");
+		}
+	}
+}
--- Unity project (Tutorial)/Assets/Midterm/scripts/enemies/AlienTypes.cs
+++ Unity project (Tutorial)/Assets/Midterm/scripts/enemies/AlienTypes.cs
@@ -5,6 +5,24 @@ public class AlienTypes : MonoBehaviour {
 
 	public int[] alienhealth;
 
+	/*void Start ()n {
+	 * while (alienhealth [0] <= 0) {
+			alienhealth [0] = 0;
+			print ("You killed Alien!");
+		}
+	 * } */
+	private PlayerHealth playerHealth;
+
+	void Awake () {
+		playerHealth = GetComponent<PlayerHealth> ();
+	}
+
+	/*void Start () {
+		WaitForSeconds (5);
+		if (alienhealth [0] >0){
+			playerHealth.health -= 1;
+		}
+	}*/
 	void Update () {
 		if (alienhealth [0] <= 0) {
 			alienhealth [0] = 0;
