3dabbea484097efa0f8f299ea3f5d787f917b6c7
brytoncl <brytonclittle@gmail.com>
2016-07-06 13:06:11 -0600

    Made ScopeAssignment Scripts

---
+++ Unity Project (Class)/Assets/Scripts/scopeAccessModifiers/ScopeAssignment1.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ScopeAssignment1 : MonoBehaviour {
+	public int apples = 10;
+
+	public int EatApples () {
+		return apples -= 1;
+	}
+}
---
+++ Unity Project (Class)/Assets/Scripts/scopeAccessModifiers/ScopeAssignment2.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ScopeAssignment2 : MonoBehaviour {
+
+	public ScopeAssignment1 GetFunction;
+
+	void Update () {
+		if (Input.GetKeyDown (KeyCode.A)) {
+			GetFunction.EatApples ();
+		}
+	}
+
+}
