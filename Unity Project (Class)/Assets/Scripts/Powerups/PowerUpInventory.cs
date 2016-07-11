3856e91351e5835d69055b5612426c7c740bc3e4
brytoncl <brytonclittle@gmail.com>
2016-07-11 11:31:00 -0600

    Powerups in Beta

---
+++ Unity Project (Class)/Assets/Scripts/Powerups/PowerUpInventory.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class PowerUpInventory : MonoBehaviour {
+
+	public List<PowerUpBase> myPowerUps = new List<PowerUpBase> ();
+
+	public void AddPowerUpToList (PowerUpBase _pub) {
+		myPowerUps.Add (_pub); 
+		i++;
+		print (_pub.name);
+	}
+	public int i = -1;
+
+	void Update () {
+		if (Input.GetKeyDown (KeyCode.A)) {
+			if (i > -1) {
+				PowerUpStatics.mySpeed += myPowerUps [i].speed;
+				myPowerUps.RemoveAt (i);
+				i--;
+				print (PowerUpStatics.mySpeed);
+			}
+		}
+	}
+}
