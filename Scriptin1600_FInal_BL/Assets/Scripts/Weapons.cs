422d39a04ffac1d510426908c65113b103dd4cf0
brytoncl <brytonclittle@gmail.com>
2016-08-02 14:14:50 -0600

    Started Final Project

---
+++ Scriptin1600_FInal_BL/Assets/Scripts/Weapons.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+	public class Weapons : MonoBehaviour {
+
+		public WeaponsInventory myWeaponsInventory;
+
+		//the game object name 
+		public int firePower;
+		public int ammoCount;
+
+		void OnMouseUp () {
+			if (myWeaponsInventory.myWeapons.Count < 2) {
+				myWeaponsInventory.myWeapons.Add (this);
+				gameObject.SetActive (false);
+			} else
+				print ("Invventory Full");
+		}
+	}
