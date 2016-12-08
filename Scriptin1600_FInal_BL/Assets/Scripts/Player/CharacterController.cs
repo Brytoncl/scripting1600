d4c0ffe2b7645e82de0c7d06a066b6ffa0c40171
brytoncl <brytonclittle@gmail.com>
2016-12-08 12:03:44 -0700

    player controller

--- Scriptin1600_FInal_BL/Assets/Scripts/Player/CharacterController.cs
+++ Scriptin1600_FInal_BL/Assets/Scripts/Player/CharacterController.cs
@@ -4,4 +4,28 @@ using System.Collections;
 public class CharacterController : MonoBehaviour {
 
 
-	}
+	private CharacterController character;
+	private Vector3 moveVector;
+	public float moveSpeed = 5;
+	public float rotateSpeed = 1;
+
+	void Start () {
+			character = GetComponent<CharacterController> ();
+			}
+	void Rotate (float _myInput) 
+	{
+			transform.Rotate (0, _myInput*rotateSpeed, 0);
+		}
+		
+			void Move (float _myInput) 
+			{
+			moveVector.x = _myInput;
+			moveVector = transform.TransformDirection (moveVector*moveSpeed*Time.deltaTime);
+			character.Move (moveVector.x); 
+			}
+		
+			void Update () {
+					Move (Input.GetAxis ("Vertical"));
+					Rotate (Input.GetAxis ("Horizontal"));
+				}
+		}
