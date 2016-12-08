d0a0ac064555ed9ffade9c067eae1bd859daf0e7
brytoncl <brytonclittle@gmail.com>
2016-12-08 12:08:16 -0700

    powerups script

--- Scriptin1600_FInal_BL/Assets/Scripts/PowerUps/PowerUps.cs
+++ Scriptin1600_FInal_BL/Assets/Scripts/PowerUps/PowerUps.cs
@@ -4,6 +4,19 @@ using System.Collections;
 public class PowerUps : MonoBehaviour {
 	//parent that gives function on collision enter, and ability()
 
+	// interface these functions into the controlls.
+
+	void OnCollisonEnter () {
+		Ability ();
+	}
+
+	public void Ability () {
+
+	}
+
+	void Start  (){
+		this.gameObject.SetActive (false);
+	}
 
 
 }
